54567af457e3667d3e6799b74b6035253a1d753e
Thomas Plisson <thomas.plisson@epitech.eu>
2022-04-12 15:29:45 +0200

    Update project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { IAccount, IProvider, ProviderType, Client, IClientConfig, ICallData, IAddressInfo } from "massa-web3";
 import { Chess } from "chess.js";
@@ -33,29 +33,42 @@ const web3ClientConfig = {
 const web3Client: Client = new Client(web3ClientConfig, baseAccount);
 // END
 
-let board: string = "";
 
-function GetBoard() {
-  web3Client
+async function GetBoard() {
+  let addr: any = await web3Client
     .publicApi()
-    .getAddresses([chessSC]).then((x: any) => {
-      board = String.fromCharCode(...x[0]['candidate_sce_ledger_info']['datastore']['2KDMgrjWrVvtv8RGy9dTxYBEGAFtuRLYUT5Qwto2Ro3gL8h8Nx']);
-      console.log("1");
-      console.log(board);
-    });
+    .getAddresses([chessSC]);
+  let board = String.fromCharCode(...addr[0]['candidate_sce_ledger_info']['datastore']['2KDMgrjWrVvtv8RGy9dTxYBEGAFtuRLYUT5Qwto2Ro3gL8h8Nx']);
+  console.log("GetBoard = " + board);
+  return board;
 }
 
 // START
 function ChessEngine() {
-  console.log("START ENGINE")
-  // note: use this
-  // GetBoard();
-  let [game, setGame] = useState(new Chess("rnbqkb1r/pppp1ppp/4p2n/8/8/4PP2/PPPP2PP/RNBQKBNR w KQkq - 1 3"));
-  console.log("2");
-  console.log(board);
+  console.log("1 ITER");
+  const [game, setGame] = useState(new Chess());
+
+  GetBoard().then(game.load);
+  console.log("THEN: ", game.fen())
+
+  // useEffect(() => {
+  //   // declare the data fetching function
+  //   const fetchData = async () => {
+  //     const data: string = await GetBoard();
+  //     console.log("BOARD");
+  //     console.log(data);
+  //     game.load(await GetBoard());
+  //     console.log("check: " + game.fen())
+  //   }
+
+  //   // call the function
+  //   fetchData()
+  //     // make sure to catch any error
+  //     .catch(console.error);
+  // }, []);
 
   function safeGameMutate(modify: any) {
-    setGame((g: any) => {
+    setGame((g) => {
       const update = { ...g };
       modify(update);
       return update;
@@ -64,9 +77,8 @@ function ChessEngine() {
 
   function makeRandomMove() {
     const possibleMoves = game.moves();
-    // exit if the game is over
     if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
-      return;
+      return; // exit if the game is over
     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
     safeGameMutate((game: any) => {
       game.move(possibleMoves[randomIndex]);
@@ -82,22 +94,19 @@ function ChessEngine() {
         promotion: "q",
       });
     });
-    if (move === null) {
-      console.log("ILLEGAL MOVE");
-      return false;
-    }
-    makeRandomMove();
-    console.log(game.fen());
-    web3Client.smartContracts().callSmartContract({
-      fee: 0,
-      maxGas: 10_000_000,
-      gasPrice: 0,
-      parallelCoins: 0,
-      sequentialCoins: 0,
-      targetAddress: chessSC,
-      functionName: "set",
-      parameter: game.fen(),
-    } as ICallData, baseAccount).then(console.log);
+    if (move === null) return false; // illegal move
+    setTimeout(makeRandomMove, 200);
+    console.log("FINAL FEN: " + game.fen());
+    // web3Client.smartContracts().callSmartContract({
+    //   fee: 0,
+    //   maxGas: 10_000_000,
+    //   gasPrice: 0,
+    //   parallelCoins: 0,
+    //   sequentialCoins: 0,
+    //   targetAddress: chessSC,
+    //   functionName: "set",
+    //   parameter: game.fen(),
+    // } as ICallData, baseAccount).then(console.log);
     return true;
   }
 
